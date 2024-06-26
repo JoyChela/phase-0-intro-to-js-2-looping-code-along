ec6d59753fe6a9b85a686d4c80aca5601cd80cd0
Joy Chelangat <joychela000@gmail.com>
2024-06-26 09:31:17 +0300

    count

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+    let thankYouMessages = [];
+
+    for (let i = 0; i < names.length; i++) {
+        let message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+        thankYouMessages.push(message);
+    }
+
+    return thankYouMessages;
+}
+
+// Example usage:
+const namesArray = ["Charlie", "Samip", "Ali"];
+const event = "birthday";
+
+const messages = writeCards(namesArray, event);
+console.log(messages);
+
+
+
+function countDown(){
+    let countDown = 10;
+    while (countDown >=0){
+        console.log(countDown);
+        countDown--;
+
+    }
+}
